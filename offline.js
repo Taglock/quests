758eb32c7c7a00d6a0a64c817608c8f8c35847f8
Taglock <39584594+Taglock@users.noreply.github.com>
2022-08-15 19:49:34 +0500

    c

---
+++ offline.js
@@ -0,0 +1,30 @@
+﻿{
+	"version": 1660574832,
+	"fileList": [
+		"data.js",
+		"game.html",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"img/sprite-sheet0.png",
+		"img/sprite-sheet1.png",
+		"img/sprite-sheet2.png",
+		"img/sprite-sheet3.png",
+		"img/chara-sheet0.png",
+		"img/chara-sheet1.png",
+		"img/chara-sheet2.png",
+		"img/chara-sheet3.png",
+		"img/bkg.png",
+		"img/ans.png",
+		"img/sprite2-sheet0.png",
+		"media/mechanical-keyboard-single-button-presses-9-www.fesliyanstudios.com.m4a",
+		"media/techno-loop.ogg",
+		"media/a78cf0d31b6ea23.ogg",
+		"media/ace-of-base-happy-nation_dj-white-man-remix_-_promodj.com_.ogg",
+		"media/без названия.m4a",
+		"media/без названия.ogg",
+		"media/indie 11-08-2022 04-54.m4a",
+		"media/indie 11-08-2022 04-54.ogg",
+		"loading-logo.png"
+	]
+}
